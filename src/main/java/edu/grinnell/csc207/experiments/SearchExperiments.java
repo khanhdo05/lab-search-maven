9537d3df512d2cd4def4cd1e8809c5fe121d0ab6
KHANH <khanhphuongdo28@gmail.com>
2024-10-29 14:22:12 -0500

    Implement search method for iterables.

--- src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/SearchExperiments.java
@@ -12,8 +12,8 @@ import edu.grinnell.csc207.util.SearchUtils;
 /**
  * Assorted experiments for searching structures.
  *
- * @author Your Name Here
- * @author Your Name Here
+ * @author Kevin Tang
+ * @author Khanh Do
  * @author Samuel A. Rebelsky (starter code)
  */
 public class SearchExperiments {
@@ -34,6 +34,32 @@ public class SearchExperiments {
                        "romeo", "sierra", "tango", "uniform",
                        "victor", "whiskey", "xray", "yankee", "zulu" };
     ArrayList<String> strings = new ArrayList<String>(Arrays.asList(tmp));
+    Predicate<String> pred = new Predicate<String>() {
+      @Override
+      public boolean test(String arg0) {
+        return (arg0.length() < 5);
+      } // test(String)
+    };
+    try {
+      String ex1c = SearchUtils.search(strings, pred);
+      pen.println("The first string of fewer than five letters is " + ex1c);
+    } catch (Exception e) {
+      pen.println("There are no strings of fewer than five letters.");
+    } // try/catch
+
+    try {
+      String ex1g = SearchUtils.search(strings, (s) -> s.length() == 6);
+      pen.println("The first string of exactly six letters is " + ex1g);
+    } catch (Exception e) {
+      pen.println("There are no strings of exactly six letters.");
+    } // try/catch
+
+    try {
+      String ex1g = SearchUtils.search(strings, (s) -> s.contains("u"));
+      pen.println("The first string that contains a u is " + ex1g);
+    } catch (Exception e) {
+      pen.println("There are no strings that contains a u.");
+    } // try/catch
 
     pen.close();
   } // main(String[])
